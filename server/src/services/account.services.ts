0d6d45e21d05edfb4ac34a6303503c7ec710f61e
Deep <deep@example.com>
2026-06-08 13:02:29 +0530

    feat: implement getLedgerEntries function for retrieving ledger entries with pagination

---
+++ server/src/services/account.services.ts
@@ -0,0 +1,48 @@
+const { pool } = require('../config/db');
+import type { LedgerEntry } from '../types/index.ts';
+
+const getLedgerEntries = async (
+    accountId: string,
+    cursor?: { createdAt: Date, id: string },
+    limit: number = 50
+) => {
+    const client = await pool.connect();
+
+    try{
+        let queryText = '';
+        let queryParams: any[] = [];
+        if (cursor) {
+            queryText = `
+                SELECT * FROM ledger_entries
+                WHERE account_id = $1
+                AND (created_at, id) < ($2, $3)
+                ORDER BY created_at DESC, id DESC
+                LIMIT $4
+            `;
+            queryParams = [accountId, cursor.createdAt, cursor.id, limit]
+        } else {
+            queryText = `
+                SELECT * FROM ledger_entires
+                WHERE account_id = $1
+                ORDER BY created_at DESC, id DESC
+                LIMIT $2
+            `;
+            queryParams = [accountId, limit]
+        }
+        const { rows } = await client.query(queryText, queryParams);
+        const entries: LedgerEntry[] = rows.map((row: { amount: string | number | bigint | boolean; }) => ({
+            ...row,
+            amount: BigInt(row.amount)
+        }));
+
+        const nextCursor = rows.length === limit?{
+            createdAt: rows[rows.length - 1].created_at,
+            id: rows[rows.length - 1].id
+        } : null
+
+        return { entries, nextCursor }
+
+    }finally{
+        client.release();
+    }
+}
